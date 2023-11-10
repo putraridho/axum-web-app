a16859c63fb7d44c265a0bae6b7407c04e9d031e
Ridho Putra <muhammadridhoputra@gmail.com>
2023-11-10 17:48:38 +0700

    Axum ticket routes

---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::net::SocketAddr;
+
+use axum::{
+    extract::{Path, Query},
+    http::{Method, Uri},
+    middleware,
+    response::{Html, IntoResponse, Response},
+    routing::{get, get_service},
+    Json, Router, Server,
+};
+use serde::Deserialize;
+use serde_json::json;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+use uuid::Uuid;
+
+use crate::{
+    ctx::Ctx,
+    error::{ClientError, Error, Result},
+    log::log_request,
+    model::ModelController,
+};
+
+mod ctx;
+mod error;
+mod log;
+mod model;
+mod web;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize ModelController.
+    let mc = ModelController::new().await?;
+
+    let routes_apis = web::routes_tickets::routes(mc.clone())
+        .route_layer(middleware::from_fn(web::mw_auth::mw_require_auth));
+
+    let routes = Router::new()
+        .merge(routes_all())
+        .merge(web::routes_login::routes())
+        .nest("/api", routes_apis)
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(
+            mc.clone(),
+            web::mw_auth::mw_ctx_resolver,
+        ))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("->> LISTENING on {addr}\n");
+    Server::bind(&addr)
+        .serve(routes.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+fn routes_all() -> Router {
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/:name", get(handler_hello2))
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+async fn main_response_mapper(
+    ctx: Option<Ctx>,
+    uri: Uri,
+    req_method: Method,
+    res: Response,
+) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    let uuid = Uuid::new_v4();
+
+    // -- Get the eventual response error.
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|se| se.client_status_and_error());
+
+    // -- If client error, build the new response.
+    let error_response = client_status_error
+        .as_ref()
+        .map(|(status_code, client_error)| {
+            let client_error_body = json!({
+                "error": {
+                    "type": client_error.as_ref(),
+                    "req_uuid": uuid.to_string(),
+                }
+            });
+
+            println!("  ->> client_error_body: {client_error_body}");
+
+            // Build the new response from the client_error_body
+            (*status_code, Json(client_error_body)).into_response()
+        });
+
+    // Build and log the server log line.
+    let client_error = client_status_error.unzip().1;
+    let _ = log_request(uuid, req_method, uri, ctx, service_error, client_error).await;
+
+    println!();
+    error_response.unwrap_or(res)
+}
+
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
